b64c0891029c4ade9a74a0804f390b8ff33bb9f8
Epic Gamer <43441605+JoriKos@users.noreply.github.com>
2020-05-12 15:23:00 +0200

    movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float speed = 10f;
+    [SerializeField] GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
