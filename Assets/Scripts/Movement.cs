f89547d72e9e42aa5f6f2da582f5b9d82b2141ef
Epic Gamer <43441605+JoriKos@users.noreply.github.com>
2020-05-12 16:45:04 +0200

    Jumping

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,31 +4,73 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    float speed = 10f;
-    [SerializeField] GameObject player;
+    private float speed = 10f;
+    private float jumpHeight = 5;
+    private bool canJump = false;
+    private bool isGrounded = true;
+    private GameObject player;
+    private Rigidbody rb;
 
     private void Awake()
     {
         player = GameObject.Find("Player");
+        isGrounded = true;
+        canJump = false;
+        rb = player.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            rb.AddForce(0, jumpHeight, 0, ForceMode.Impulse);
+        }
     }
 
     private void Update()
     {
+        if (isGrounded)
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                canJump = true;
+            }
+        }
         if (Input.GetKey(KeyCode.W))
         {
             player.transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
+
         if (Input.GetKey(KeyCode.S))
         {
             player.transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             player.transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
+
         if (Input.GetKey(KeyCode.D))
         {
             player.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
         }
     }
 }
